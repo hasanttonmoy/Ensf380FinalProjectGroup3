82aa5ac2b66f7afab3c644a692f7e93b0037cfc1
OwenGuldberg <guldbergowen@gmail.com>
2023-08-06 20:46:38 -0600

    Added documentation and few modifications
    Documented the GetStationData class, the NewsFetcher class, the
    TrainArray class, and the TrainMapCreator class. Also slightly
    simplified classes and removed or changed redundant code.

--- src/ca/ucalgary/edu/ensf380/TrainArray.java
+++ src/ca/ucalgary/edu/ensf380/TrainArray.java
@@ -8,7 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.*;
 
+/*
+ * The TrainArray class is used to create an array of trains that stores data generated from the SubwaySimulator
+ * 
+ * @author Owen Guldberg
+ * @version 2023-08-06
+ */
 public class TrainArray {
+	/*
+	 * A train object that stores data generated from the SubwaySimulator
+	 */
     public static class Train {
         private String lineName;
         private int trainNumber;
@@ -21,10 +30,20 @@ public class TrainArray {
 		private String currentStationName;
 		private String nextStationName;
 		private String prevStationName;
-		
 		private String currentStationXCord;
 		private String currentStationYCord;
-
+		
+		/**
+		 * Creates the train object
+		 * 
+		 * @param lineName
+		 * @param trainNumber
+		 * @param stationCode
+		 * @param direction
+		 * @param destination
+		 * @param nextStation
+		 * @param prevStation
+		 */
         public Train(String lineName, int trainNumber, String stationCode, String direction, String destination, String nextStation, String prevStation) {
             this.lineName = lineName;
             this.trainNumber = trainNumber;
@@ -40,43 +59,32 @@ public class TrainArray {
 			this.currentStationYCord = GetStationData.getYCordByCode(stationCode);
 			
         }
-
-
+        
+        // Getters
         public String getLineName() {
             return lineName;
         }
-
-
         public int getTrainNumber() {
             return trainNumber;
         }
-
-
         public String getStationCode() {
             return stationCode;
         }
-
-
         public String getDirection() {
             return direction;
         }
-
-
         public String getDestination() {
             return destination;
         }
-		
 		public int getTrainXCord() {
 			return (int) Float.parseFloat(currentStationXCord);
 		}
-		
 		public int getTrainYCord() {
 			return (int) Float.parseFloat(currentStationYCord);
 		}
-		
 
         @Override
-        public String toString() {
+        public String toString() { // Used for debugging
             return "Train{" +
                     "lineName='" + lineName + '\'' +
                     ", trainNumber=" + trainNumber +
@@ -93,33 +101,45 @@ public class TrainArray {
                     '}';
         }
     }
-	
-
+    
+    /**
+     * This method is used to parse the SubwaySimulator csv file output and then create an array of train objects from the data
+     * @return An array of train objects that stores the data
+     */
 	public static Train[] parseCsvFile() {
+		// Create the array list
 		List<Train> trains = new ArrayList<>();
 		
+		// Read the most recently created csv file that is in the out folder
 		String currentDirectory = System.getProperty("user.dir");
 		String outputPath = currentDirectory + File.separator + "out";
 		File newestfile = getLastModified(outputPath);
 		String lastpath = outputPath + "\\" + newestfile.getName();
-
 		try (BufferedReader br = new BufferedReader(new FileReader(lastpath))) {
 			String line;
+			
+			// Skip the header
 			boolean firstLine = true;
 			while ((line = br.readLine()) != null) {
 				if (firstLine) {
 					firstLine = false;
 					continue;
 				}
+				
+				// Delimiter
 				String[] data = line.split(",");
 				if (data.length == 5) {
+					
+					// Save all the data for a train object
 					String lineName = data[0].trim();
 					int trainNumber = Integer.parseInt(data[1].trim());
 					String stationCode = data[2].trim();
 					String direction = data[3].trim();
 					String destination = data[4].trim();
-					
 					String input = stationCode;
+					
+					// Get the station code of the prev and next train station depending on train direction.
+					// Also split the station code to get "R" and an int for the station number
 					String regex = "(\\d+)$";
 					Pattern pattern = Pattern.compile(regex);
 					Matcher matcher = pattern.matcher(input);
@@ -152,39 +172,46 @@ public class TrainArray {
 						prevCode = 120;
 					}
 					
+					// Turn the station values back into Strings
 					String nextStation = input.replaceAll(regex, String.format("%02d", nextCode));
 					String prevStation = input.replaceAll(regex, String.format("%02d", prevCode));
-
+					
+					// Create the train object and add it to the list
 					Train train = new Train(lineName, trainNumber, stationCode, direction, destination, nextStation, prevStation);
 					trains.add(train);
 				}
 			}
+			
 		} catch (IOException | NumberFormatException e) {
 			e.printStackTrace();
 		}
-
 		return trains.toArray(new Train[0]);
 	}
 	
+	/**
+	 * Finds the path of the most recently created file in a directory
+	 * 
+	 * @param directoryFilePath
+	 * @return The path of the newest file
+	 */
 	public static File getLastModified(String directoryFilePath) {
 
 	    File directory = new File(directoryFilePath);
+	    // Create array of all the files in the directory
 	    File[] files = directory.listFiles(File::isFile);
-	    long lastModifiedTime = Long.MIN_VALUE;
-	    File chosenFile = null;
-
-	    if (files != null) {
-
-				for (File file : files)
-				{
-					if (file.lastModified() > lastModifiedTime)
-					{
+	    long lastModifiedTime = Long.MIN_VALUE; // Represents how long ago the file was created
+	    File chosenFile = null; // file object for the newest file
+	    
+	    // Check that directory was not empty
+	    if (null != files) {
+	    		// Finds the newest file
+				for (File file : files) {
+					if (file.lastModified() > lastModifiedTime) {
 						chosenFile = file;
 						lastModifiedTime = file.lastModified();
 					}
 				}
 			}
-	    	System.out.println(lastModifiedTime);
 			return chosenFile;
 		}
 
